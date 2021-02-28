3bf11b4111437358e3fd294d94ef89a98b44db43
Gabriel <dev.gabrielramos@gmail.com>
2021-02-27 23:36:16 -0300

    :rocket: Rumo à fluência - Revisão

---
+++ Etapa 01 - Primeiros passos/Aula 01/06. Constantes, variáveis e comentários/app.js
@@ -0,0 +1,19 @@
+/*
+Comentário multilinha
+*/
+
+// let age = 31
+/*
+let currentYear = 2019
+
+console.log(age, currentYear)
+
+age = 32
+console.log(age)
+
+const points = 100
+console.log(points)
+*/
+
+var score = 75
+console.log(score)
