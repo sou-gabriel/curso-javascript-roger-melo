b0cb7edf670ba2b7bb878e2e781c24ccfd556c90
Gabriel Ramos Nogueira <69519457+sou-gabriel@users.noreply.github.com>
2021-03-19 16:11:33 -0300

    :rocket: Rumo à fluência - Revisão

---
+++ Etapa 05 - DOM/Aula 01/08. Outras maneiras de fazer queries no DOM/app.js
@@ -0,0 +1,14 @@
+// obter um elemento através do ID
+// const title = document.getElementById('title')
+
+// console.log(title)
+
+// obter elementos através do nome da classe
+// const errors = document.getElementsByClassName('error')
+
+// console.log(errors)
+
+// obter elementos através do nome da tag
+const paragraphs = document.getElementsByTagName('p')
+
+console.log(paragraphs)
